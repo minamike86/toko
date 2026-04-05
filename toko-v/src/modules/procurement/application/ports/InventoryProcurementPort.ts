6bfa35815220227a04a55bf95d8808e93233602e
minamike86 <64697999+minamike86@users.noreply.github.com>
2026-04-05 20:29:29 +0700

    Add procurement inventory port

---
+++ toko-v/src/modules/procurement/application/ports/InventoryProcurementPort.ts
@@ -0,0 +1,10 @@
+export type ReceiveProcurementStockRequest = {
+  variantId: string;
+  quantity: number;
+  reason: string;
+  referenceId: string;
+};
+
+export interface InventoryProcurementPort {
+  receivePurchaseStock(requests: ReceiveProcurementStockRequest[]): Promise<void>;
+}
